099262c21a3d44e07bbc843ea764cf0392c08031
todddddd <withers.todd@gmail.com>
2017-04-13 11:48:17 -0700

    removing unused packet files

--- Lib K Relay/Networking/Packets/Server/EvolvedPetPacket.cs
+++ Lib K Relay/Networking/Packets/Server/EvolvedPetPacket.cs
@@ -6,21 +6,26 @@ using System.Threading.Tasks;
 
 namespace Lib_K_Relay.Networking.Packets.Server
 {
-    public class UpdatePetPacket : Packet
+    public class EvolvedPetPacket : Packet
     {
         public int PetId;
-
+        public int SkinId1;
+        public int SkinId2;
         public override PacketType Type
-        { get { return PacketType.UPDATEPET; } }
+        { get { return PacketType.EVOLVEPET; } }
 
         public override void Read(PacketReader r)
         {
             PetId = r.ReadInt32();
+            SkinId1 = r.ReadInt32();
+            SkinId2 = r.ReadInt32();
         }
 
         public override void Write(PacketWriter w)
         {
             w.Write(PetId);
+            w.Write(SkinId1);
+            w.Write(SkinId2);
         }
     }
 }
